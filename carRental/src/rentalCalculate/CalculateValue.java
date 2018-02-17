ed7fff43a44c99590cb5b09c2b03a97208036db3
Fabiano Júnior <fabiano.lomonaco.junior@gmail.com>
2018-02-17 13:48:36 -0300

    Fixing de function and validate to calculate value of rent

--- carRental/src/rentalCalculate/CalculateValue.java
+++ carRental/src/rentalCalculate/CalculateValue.java
@@ -5,66 +5,69 @@ public class CalculateValue {
 	private String resultRegular;
 	private String resultFidelity;
 	private String finalResult;
+	private float auxVarCalcReg;
+	private float auxVarCalcFid;
 	
 	public String calcValue(String model, int initDate, int finalDate){
 		
-		switch(model){
+		if (model == "Compact-Car"){
+			
+			//weekend
+			if(initDate == 0 || initDate == 6 && finalDate == 0 || finalDate == 6){
+				resultRegular = ("R$ 200");
+				resultFidelity = ("R$ 90");
+				finalResult = ("**SouthCar**" + "\n" + "REGULAR:"+ resultRegular + "\n" + "FIDELITY:" + resultFidelity);
+				
+			}
+			
+			//not weekend
+			else {
+						
+				auxVarCalcReg = (finalDate - initDate) * 210;
+				auxVarCalcFid = (finalDate - initDate) * 150;
+				finalResult = ("**SouthCar**" + "\n" + "REGULAR: R$"+ auxVarCalcReg + "\n" + "FIDELITY: R$" + auxVarCalcFid);
+			
+			}
+		}
 		
-		case "Compact-Car":
+		if (model == "Sports-Car"){
 			
-			//one day
-			if(initDate == finalDate){
-				//weekend
-				if(initDate == 0 || initDate == 6 && finalDate == 0 || finalDate == 6){
-					resultRegular = ("R$ 200,00 ");
-					resultFidelity = ("R$ 90,00 ");
-					finalResult = ("**SouthCar**" + "\n" + "REGULAR:"+ resultRegular + "\n" + "FIDELITY:" + resultFidelity);
-					break;
-				}
-				
-				else{
-					
-				}
+			//weekend
+			if(initDate == 0 || initDate == 6 && finalDate == 0 || finalDate == 6){
+				resultRegular = ("R$ 200");
+				resultFidelity = ("R$ 90");
+				finalResult = ("**WestCar**" + "\n" + "REGULAR:"+ resultRegular + "\n" + "FIDELITY:" + resultFidelity);
 				
 			}
 			
-		case "Sports-Car":
+			//not weekend
+			else {
+						
+				auxVarCalcReg = (finalDate - initDate) * 530;
+				auxVarCalcFid = (finalDate - initDate) * 150;
+				finalResult = ("**SouthCar**" + "\n" + "REGULAR: R$"+ auxVarCalcReg + "\n" + "FIDELITY: R$" + auxVarCalcFid);
 			
-			//one day
-			if(initDate == finalDate){
-				//weekend
-				if(initDate == 0 || initDate == 6 && finalDate == 0 || finalDate == 6){
-					resultRegular = ("R$ 200,00 ");
-					resultFidelity = ("R$ 90,00 ");
-					finalResult = ("**WestCar**" + "\n" + "REGULAR:"+ resultRegular + "\n" + "FIDELITY:" + resultFidelity);
-					break;
-				}
-				
-				else{
-					
-				}
-				
 			}
-			
-		case "SUV's":
-			
-			//one day
-			if(initDate == finalDate){
-				//weekend
-				if(initDate == 0 || initDate == 6 && finalDate == 0 || finalDate == 6){
-					resultRegular = ("R$ 600,00 ");
-					resultFidelity = ("R$ 590,00 ");
-					finalResult = ("**NorthCar**" + "\n" + "REGULAR:"+ resultRegular + "\n" + "FIDELITY:" + resultFidelity);
-					break;
-				}
-				
-				else{
-					
-				}
-				
-			}
-			
+		}
 		
+		if (model == "SUV's"){
+			
+			//weekend
+			if(initDate == 0 || initDate == 6 && finalDate == 0 || finalDate == 6){
+				resultRegular = ("R$ 600");
+				resultFidelity = ("R$ 590");
+				finalResult = ("**SouthCar**" + "\n" + "REGULAR:"+ resultRegular + "\n" + "FIDELITY:" + resultFidelity);
+				
+			}
+			
+			//not weekend
+			else {
+						
+				auxVarCalcReg = (finalDate - initDate) * 630;
+				auxVarCalcFid = (finalDate - initDate) * 580;
+				finalResult = ("**SouthCar**" + "\n" + "REGULAR: R$"+ auxVarCalcReg + "\n" + "FIDELITY: R$" + auxVarCalcFid);
+			
+			}
 		}
 		
 		return finalResult; 
